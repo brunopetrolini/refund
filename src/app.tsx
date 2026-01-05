1a419072b9d8d029a9a011ae50cfbb803ae6f0ef
Bruno Petrolini <bruno.petrolini@outlook.com.br>
2026-01-05 20:36:07 -0300

    feat: integrate Biome for formatting and linting

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,7 @@
 export function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-zinc-900 text-zinc-50">Hello, Refund!</div>
-  )
+    <div className="flex h-screen items-center justify-center bg-zinc-900 text-zinc-50">
+      Hello, Refund!
+    </div>
+  );
 }
