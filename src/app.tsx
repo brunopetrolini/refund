f7042732e43d3ee93f6db060cb1f7c7372b8da1c
Bruno Petrolini <bruno.petrolini@outlook.com.br>
2026-01-05 20:25:48 -0300

    feat: integrate Tailwind CSS into the React application

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 export function App() {
   return (
-    <div>Hello, Refund!</div>
+    <div className="flex items-center justify-center h-screen bg-zinc-900 text-zinc-50">Hello, Refund!</div>
   )
 }
